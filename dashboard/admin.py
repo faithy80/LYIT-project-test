f7b16c8a0b8e09e91b49de12f011faa791cadb13
Krisztian Buza <krisztian.buza80@gmail.com>
2021-08-02 16:18:44 +0000

    Site setting singleton model added

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import TempHistory
+from .models import SiteSettings
 
 
 class TempHistortyAdmin(admin.ModelAdmin):
     readonly_fields = ('temp_date',)
 
 admin.site.register(TempHistory, TempHistortyAdmin)
+admin.site.register(SiteSettings)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class TempHistory(models.Model):
     """
     Model definition for temperature history
@@ -18,3 +19,44 @@ class TempHistory(models.Model):
         """
 
         return str(self.temp_date)
+
+
+class SingletonModel(models.Model):
+    """
+    Abstract definition for a singleton model
+    """
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        super(SingletonModel, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        pass
+
+    @classmethod
+    def load(cls):
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
+
+
+class SiteSettings(SingletonModel):
+    """
+    Model definition for site settings
+    """
+
+    temp_limit = models.FloatField('Temperature limit', default=20.0)
+    temp_offset = models.FloatField('Temperature offset', default=1.0)
+
+    class Meta:
+        verbose_name = 'Site settings'
+        verbose_name_plural = 'Site settings'
+
+    def __str__(self):
+        """
+        Unicode representation of site settings
+        """
+
+        return 'Site settings'
