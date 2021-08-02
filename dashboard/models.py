693f8f4cb9e08ab6743cf2333afd3208014a8a02
Krisztian Buza <krisztian.buza80@gmail.com>
2021-08-02 15:55:41 +0000

    System stores the incoming temperature data in the database

--- dashboard/models.py
+++ dashboard/models.py
@@ -5,8 +5,8 @@ class TempHistory(models.Model):
     Model definition for temperature history
     """
 
-    temp_data = models.DecimalField('Temperature', decimal_places=2, max_digits=2)
-    temp_date = models.DateField(auto_now_add=True)
+    temp_data = models.FloatField('Temperature')
+    temp_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name = 'Temperature'
@@ -17,4 +17,4 @@ class TempHistory(models.Model):
         Unicode representation of temperature history
         """
 
-        return self.temp_date
+        return str(self.temp_date)
--- home/mqtt.py
+++ home/mqtt.py
@@ -1,5 +1,11 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home.settings")
+
+import django
+django.setup()
+
 import paho.mqtt.client as mqtt
-from datetime import datetime
+from dashboard.models import TempHistory
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -8,14 +14,11 @@ def on_connect(client, userdata, flags, rc):
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe("esp/dht/temperature")
-    client.subscribe("esp/dht/humidity")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    # print(msg.topic+" "+str(msg.payload))
-    messages = open('messages.txt', 'a')
-    messages.writelines(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") +" "+ msg.topic + " " + str(msg.payload) + "\n")
-    messages.close()
+    temp_data = TempHistory(temp_data=float(msg.payload))
+    temp_data.save()
 
 client = mqtt.Client(client_id="DJANGO",
                      clean_session=True, userdata=None,
