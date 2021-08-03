f07ced2154d18fe37d503284bca7aed989eedcb8
Krisztian Buza <krisztian.buza80@gmail.com>
2021-08-03 18:23:10 +0100

    Site settings can be changed from dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import dashboard
+from .views import dashboard,save_site_settings
 
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('accounts/profile/', dashboard, name='dashboard'),
+    path('accounts/profile/save', save_site_settings, name='save_site_settings'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, reverse
 from django.contrib.auth.decorators import login_required
 from .models import SiteSettings, TempHistory
 from .forms import SiteSettingsForm
@@ -13,7 +13,11 @@ def dashboard(request):
 
     site_settings = SiteSettings.load()
 
-    form = SiteSettingsForm()
+    form = SiteSettingsForm({
+        'temp_limit': site_settings.temp_limit,
+        'temp_offset': site_settings.temp_offset,
+        'auto_mode': site_settings.auto_mode,
+    })
 
     historical_temp = TempHistory.objects.all().order_by('temp_date')
 
@@ -25,3 +29,22 @@ def dashboard(request):
     }
 
     return render(request, 'dashboard.html', context)
+
+def save_site_settings(request):
+    if request.method == 'POST':
+        old_settings = SiteSettings.load()
+
+        new_settings = SiteSettings()
+        new_settings.temp_limit = float(request.POST['temp_limit'])
+        new_settings.temp_offset = float(request.POST['temp_offset'])
+        new_settings.relay_state = bool(old_settings.relay_state)
+        
+        if 'auto_mode' in request.POST:
+            new_settings.auto_mode = True
+
+        else:
+            new_settings.auto_mode = False
+        
+        new_settings.save()
+    
+    return redirect(reverse('dashboard'))
