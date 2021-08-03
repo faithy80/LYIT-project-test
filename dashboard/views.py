f909ec1972d779db53ec375775f6814344ade296
Krisztian Buza <krisztian.buza80@gmail.com>
2021-08-03 16:58:29 +0100

    The corresponding data added to the dashboard view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import SiteSettings, TempHistory
+from .forms import SiteSettingsForm
 
 @login_required
 def dashboard(request):
@@ -7,4 +9,19 @@ def dashboard(request):
     Renders the dashboard page
     """
 
-    return render(request, 'dashboard.html')
+    actual_temp = TempHistory.objects.latest('temp_date')
+
+    site_settings = SiteSettings.load()
+
+    form = SiteSettingsForm()
+
+    historical_temp = TempHistory.objects.all().order_by('temp_date')
+
+    context = {
+        'actual_temp': actual_temp,
+        'site_settings': site_settings,
+        'form': form,
+        'historical_temp': historical_temp,
+    }
+
+    return render(request, 'dashboard.html', context)
