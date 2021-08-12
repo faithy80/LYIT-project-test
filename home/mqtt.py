6109e41e806d81f77810112ddd9229376c106ef5
Krisztian Buza <krisztian.buza80@gmail.com>
2021-08-12 04:44:58 +0100

    Fix Django warning on first migration added

--- home/mqtt.py
+++ home/mqtt.py
@@ -10,8 +10,6 @@ from dashboard.models import TempHistory
 from dashboard.models import SiteSettings
 from dashboard.views import mqtt_publish
 
-# Create the default setting if not created already
-SiteSettings.load()
 
 # The callback for when the client receives a CONNACK response from the server
 def on_connect(client, userdata, flags, rc):
@@ -20,49 +18,55 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server
 def on_message(client, userdata, msg):
-    # Fetch the actual temperature data from the MQTT message
-    temp_data = TempHistory(temp_data=float(msg.payload))
+    # Fix the Django warning on first migration
+    try:
+        # Fetch the actual temperature data from the MQTT message
+        temp_data = TempHistory(temp_data=float(msg.payload))
 
-    # Store the data with a timestamp in the database
-    temp_data.save()
+        # Store the data with a timestamp in the database
+        temp_data.save()
 
-    # Get actual site settings
-    site_settings = SiteSettings.load()
+        # Get actual site settings
+        site_settings = SiteSettings.load()
 
-    # If the automatic mode is enabled
-    if site_settings.auto_mode:    
-        # If the relay state is OFF and the actual temperature is below the limit
-        if not site_settings.relay_state and temp_data.temp_data < site_settings.temp_limit:
-            # Change the relay state
-            site_settings.relay_state = True
+        # If the automatic mode is enabled
+        if site_settings.auto_mode:    
+            # If the relay state is OFF and the actual temperature is below the limit
+            if not site_settings.relay_state and temp_data.temp_data < site_settings.temp_limit:
+                # Change the relay state
+                site_settings.relay_state = True
 
-            # Store the new relay state
-            site_settings.save()
+                # Store the new relay state
+                site_settings.save()
 
-            # Call the helper function to publish the ON MQTT message
-            mqtt_publish('esp/relay', 'ON')
-
-        # If the relay state is ON and the actual temperature is above the limit + the offset
-        elif site_settings.relay_state and temp_data.temp_data > site_settings.temp_limit + site_settings.temp_offset:
-            # Change the relay state
-            site_settings.relay_state = False
-
-            # Store the new relay state
-            site_settings.save()
-
-            # Call the helper function to publish the OFF MQTT message
-            mqtt_publish('esp/relay', 'OFF')
-
-        # In any other cases
-        else:
-            # Refresh relay state (resend MQTT messages to make sure that the system is alive even if the NodeMCU board was restarted)
-            if site_settings.relay_state:
-                # send ON MQTT message if the relay state is true in the database
+                # Call the helper function to publish the ON MQTT message
                 mqtt_publish('esp/relay', 'ON')
-            
-            else:
-                # send OFF MQTT message if the relay state is false in the database
+
+            # If the relay state is ON and the actual temperature is above the limit + the offset
+            elif site_settings.relay_state and temp_data.temp_data > site_settings.temp_limit + site_settings.temp_offset:
+                # Change the relay state
+                site_settings.relay_state = False
+
+                # Store the new relay state
+                site_settings.save()
+
+                # Call the helper function to publish the OFF MQTT message
                 mqtt_publish('esp/relay', 'OFF')
+
+            # In any other cases
+            else:
+                # Refresh relay state (resend MQTT messages to make sure that the system is alive even if the NodeMCU board was restarted)
+                if site_settings.relay_state:
+                    # send ON MQTT message if the relay state is true in the database
+                    mqtt_publish('esp/relay', 'ON')
+            
+                else:
+                    # send OFF MQTT message if the relay state is false in the database
+                    mqtt_publish('esp/relay', 'OFF')
+
+    except:
+        # Skip storing the temperature and control function on error
+        pass
 
 # Initialise MQTT Client
 client = mqtt.Client(client_id="DJANGO",
