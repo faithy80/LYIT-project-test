c0db14f134dd0fde8788b6de63a930ce75c49986
Krisztian Buza <krisztian.buza80@gmail.com>
2021-08-19 13:10:30 +0100

    A simple test case created

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import TempHistory, SiteSettings
+
 
 # Create your tests here.
+class TempHistoryTestCase(TestCase):
+    
+    def test_temp_history_creation(self):
+        print('This is a test!')
